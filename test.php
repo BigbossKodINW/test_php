25134f398fbf47f8573512bb548c26cb93eeddbe
Natchanon Hongkong <ho.natchanon_st@tni.ac.th>
2020-05-20 01:32:09 +0700

    initial commit

---
+++ test.php
@@ -0,0 +1,26 @@
+<DOCTYPE !html>
+<html>
+<head>
+    <title>PHP test</title>
+</head>
+<body>
+
+    <h1>Fuck OFFFFF!</h1>
+
+<?php
+$score = 50 ;
+
+if($score > 10)
+{
+    echo "You right. ".$score ;
+}
+else
+{
+    echo "You wrong." ;
+}
+
+?>
+
+</body>
+
+</html>
